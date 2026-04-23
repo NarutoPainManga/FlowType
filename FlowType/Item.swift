c96167a0b9e3ff3b5ac21ebabb52ffe40591d7c6
ryheem <ryheem@kr3pc.com>
2026-04-22 22:37:08 -0500

    Initial Commit

---
+++ FlowType/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FlowType
+//
+//  Created by Pain on 4/22/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
